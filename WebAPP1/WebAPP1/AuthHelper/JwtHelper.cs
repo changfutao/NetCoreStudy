ea78ce58e27e63228e5aad3f1f8bb56faaedc6e6
changfutao <changfutao1989@126.com>
2019-07-03 08:52:33 +0800

    111

---
+++ WebAPP1/WebAPP1/AuthHelper/JwtHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPP1.AuthHelper
+{
+    public class JwtHelper
+    {
+        public string AA { get; set; }
+    }
+}
