308ea7a58c87800ad7eb53198933712a02a26736
changfutao <changfutao1989@126.com>
2019-07-05 10:03:43 +0800

    111

---
+++ WebAPP1/WebAPP1/Filter/GlobalExceptionsFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPP1.Common.LogHelper;
+
+namespace WebAPP1.Filter
+{
+    public class GlobalExceptionsFilter : IExceptionFilter
+    {
+        private readonly IHostingEnvironment _env;
+        private readonly ILoggerHelper _loggerHelper;
+        //private readonly IHubContext<ChatHub> _hubContext;
+        public void OnException(ExceptionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
