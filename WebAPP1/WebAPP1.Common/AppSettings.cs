770e1ae860d5403327f298f6479e55290b95661f
changfutao <changfutao1989@126.com>
2019-07-06 21:00:40 +0800

    111

---
+++ WebAPP1/WebAPP1.Common/AppSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace WebAPP1.Common
+{
+    public class AppSettings
+    {
+        static IConfiguration Configuration;
+        static AppSettings()
+        {
+            string Path = "appsettings.json";
+            Configuration = new ConfigurationBuilder().AddJsonFile(Path);
+        }
+    }
+}
---
+++ WebAPP1/WebAPP1/AuthHelper/JwtHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPP1.Models;
+
+namespace WebAPP1.AuthHelper
+{
+    public class JwtHelper
+    {
+        public static string IssueJwt(TokenModelJwt tokenModelJwt)
+        {
+            
+        }
+    }
+}
